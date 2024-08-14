d629a0db9114ec4cfecd533d1cb1c895afcc0ef7
deepakness <deepakkr342@gmail.com>
2024-08-14 14:40:57 +0530

    Updated social links format

--- _data/metadata.js
+++ _data/metadata.js
@@ -7,16 +7,18 @@ module.exports = {
 
   author: {
     name: "John Doe",
+    
     // email: "john@doe.com",
     url: "https://johndoe.com/about/",
     photo: "/assets/images/author.jpg",
+
     // Social media handles - remove or comment out any you don't use
-    x: "johndoe",
+    x: "https://www.x.com/johndoe",
     github: "https://github.com/deepakness/crispwrite",
     // linkedin: "johndoe",
-    youtube: "johndoe",
-    // facebook: "johndoe",
-    // instagram: "johndoe",
-    // pinterest: "johndoe",
+    youtube: "https://www.youtube.com/johndoe",
+    // facebook: "https://www.facebook.com/johndoe",
+    // instagram: "https://www.instagram.com/johndoe",
+    // pinterest: "https://www.pinterest.com/johndoe",
   }
 };
